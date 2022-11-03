ff6defed61fb1f55d5ec00a371d7702ac9f8455e
Owen Voke <development@voke.dev>
2022-11-03 09:17:44 +0000

    feat: remove unused services from code

--- README.md
+++ README.md
@@ -1,29 +1,37 @@
-# hass-google_keep
-Custom component for [Home Assistant](https://home-assistant.io/) to enable adding to and updating lists on [Google Keep](https://keep.google.com/).
+# HASS Google Keep (Sync)
+
+Custom component for [Home Assistant](https://home-assistant.io) to enable adding to and updating lists on [Google Keep](https://keep.google.com).
 
 ## Installation
+
 Add the `gkeep` folder and its contents to the `custom_components` folder in your Home Assistant configuration directory, and add the `gkeep` component to your `configuration.yaml` file.
 
-### Example configuration.yaml entry
+### Example `configuration.yaml` entry
+
 ```yaml
-gkeep:
-  username: "this_is_my_username@gmail.com"
-  password: "this_is_my_Google_App_password"
-  list_name: "Grocery"
+gkeep_sync:
+  username: 'this_is_my_username@gmail.com'
+  password: 'this_is_my_Google_App_password'
+  list_name: 'Food Shopping'
 ```
+
 `list_name` is an optional configuration key that sets a default name for the Keep list to update.
 
 ### Dependencies
+
 This component relies on [gkeepapi](https://github.com/kiwiz/gkeepapi), an unofficial client for the Google Keep API.
 
 ## Usage
+
 The original intended use of this component was to restore the capability of Google Assistant to add things to Google Keep lists.
 I accomplish this with a combination of this custom component running on Home Assistant and [IFTTT](https://ifttt.com/).
 
 ### Home Assistant service
+
 With this custom component loaded, two services named `gkeep.add_to_list` and `gkeep.sync_shopping_list` are available.
 
 #### Add to List
+
 This service call has two data inputs: `title` and `things`, where `title` is the title of the Google Keep list to update, and `things` is a either a list or string of things to add to the list.
 A string input for `things` is parsed for multiple things separated by 'and' and/or commas.
 
@@ -43,78 +51,4 @@ automation:
       data:
         title: 'Home Supplies'
         things: 'Batteries for {{ trigger.to_state.name }}.'
-```
-
-#### Sync Shopping List
-If the Home Assistant Shopping List integration is enabled, you can use this service to synchronize your google keep list into the home assistant shopping list.
-This service call has just one data input: `title` that is the Google Keep list to sync.
-
-Here is an example of using the service in an automation to sync the list after adding an items:
-```yaml
-automation:
-  - alias: Low Battery Notification
-    trigger:
-      - platform: numeric_state
-        entity_id:
-        - sensor.front_door_battery
-        - sensor.hallway_smoke_co_alarm_battery
-        - sensor.bedroom_sensor_battery
-        below: 20
-    action:
-      - service: gkeep.add_to_list
-        data:
-          title: 'Home Supplies'
-          things: 'Batteries for {{ trigger.to_state.name }}.'
-      - delay:
-        seconds: 10
-      - service: gkeep.sync_shopping_list
-        data:
-          title: 'Home Supplies'
-```
-
-Another approach could be to schedule the sync once in a while:
-```yaml
-automation:
-  - alias: Sync google keep
-    trigger:
-      - platform: time_pattern
-        hours: "07"
-        minutes: 0
-        seconds: 0
-    action:
-      - service: gkeep.sync_shopping_list
-        data:
-          title: 'Home Supplies'
-```
-
-### IFTTT applet and Home Assistant automation
-A combination of the [Google Assistant](https://ifttt.com/google_assistant) trigger and the [Webhooks](https://ifttt.com/maker_webhooks) action is used to trigger the new Home Assistant service via Google Assistant.
-One IFTTT applet must be made per Google Keep list of interest, with the list name (e.g., 'Grocery' in the example below) hardcoded into the applet.
-For example:
-
-**IF**: Google Assistant/Say a phrase with a text ingredient  
-- *What do you want to say?*: `Add $ to the grocery list`
-- *What do you want the Assistant to say in response?*: `Okay, adding $ to your grocery list`
-
-**THEN**: Webhooks/Make a web request  
-- *URL*: `https://thisismyhassurl.org/api/webhook/ABCXYZ123456`
-- *Method*: `POST`
-- *Content Type*: `application/json`
-- *Body*: `{ "action":"call_service", "service":"gkeep.add_to_list", "title":"Grocery", "things":"{{TextField}}" }`
-
-A Home Assistant automation to receive and process Google Assistant inputs via IFTTT can have the form:
-
-```yaml
-automation:
-  - alias: Google Keep list update
-    trigger:
-      platform: event
-      event_type: ifttt_webhook_received
-      event_data:
-        action: call_service
-    action:
-      service_template: '{{ trigger.event.data.service }}'
-      data_template:
-        title: '{{ trigger.event.data.title }}'
-        things: '{{ trigger.event.data.things }}'
 ```
--- custom_components/gkeep/__init__.py
+++ custom_components/gkeep/__init__.py
@@ -5,14 +5,14 @@ Google Keep API (https://github.com/kiwiz/gkeepapi).
 
 Example configuration.yaml entry:
 
-gkeep:
+gkeep_sync:
   username: 'this_is_my_username@gmail.com'
   password: 'this_is_my_Google_App_password'
 
 With this custom component loaded, a new service named google_keep.add_to_list
-is available. This service data call has two inputs: 'title' and 'things', where
-'title' is the title of the Google Keep list, and 'things' is a either a list of
-things, or a string. A string input for 'things' is parsed for multiple things
+is available. This service data call has two inputs: 'title' and 'items', where
+'title' is the title of the Google Keep list, and 'items' is a either a list of
+items, or a string. A string input for 'items' is parsed for multiple items
 separated by 'and'.
 """
 
@@ -26,12 +26,11 @@ from homeassistant.core import callback
 _LOGGER = logging.getLogger(__name__)
 
 # Domain and component constants and validation
-DOMAIN = "gkeep"
-SHOPPING_LIST_DOMAIN = "shopping_list"
-CONF_USERNAME = 'username'    # Google account username
-CONF_PASSWORD = 'password'    # Google App password, https://myaccount.google.com/apppasswords
+DOMAIN = "gkeep_sync"
+CONF_USERNAME = 'username'  # Google account username
+CONF_PASSWORD = 'password'  # Google App password, https://myaccount.google.com/apppasswords
 CONF_LIST_NAME = 'list_name'  # Default Google Keep list title
-DEFAULT_LIST_NAME = 'Grocery'
+DEFAULT_LIST_NAME = 'Food Shopping'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -42,8 +41,8 @@ CONFIG_SCHEMA = vol.Schema({
 }, extra=vol.ALLOW_EXTRA)
 
 # Service constants and validation
-SERVICE_LIST_NAME = 'title'   # Title of the Google Keep list to create or update, string
-SERVICE_LIST_ITEM = 'things'  # Things(s) to add to the list
+SERVICE_LIST_NAME = 'name'  # Title of the Google Keep list to create or update, string
+SERVICE_LIST_ITEM = 'items'  # items(s) to add to the list
 
 SERVICE_LIST_SCHEMA = vol.Schema({
     vol.Optional(SERVICE_LIST_NAME): cv.string,
@@ -54,8 +53,9 @@ SERVICE_LIST_NAME_SCHEMA = vol.Schema({
     vol.Optional(SERVICE_LIST_NAME): cv.string,
 })
 
+
 def setup(hass, config):
-    """Setup the google_keep component."""
+    """Set up the gkeep_sync component."""
 
     import gkeepapi
 
@@ -67,107 +67,59 @@ def setup(hass, config):
 
     keep = gkeepapi.Keep()
 
-    # Attempt to login
+    # Attempt to log in
     login_success = keep.login(username, password)
     if not login_success:
         _LOGGER.error("Google Keep login failed.")
         return False
 
     def add_to_list(call):
-        """Add things to a Google Keep list."""
+        """Add items to a Google Keep list."""
 
         list_name = call.data.get(SERVICE_LIST_NAME, default_list_name)
-        things = call.data.get(SERVICE_LIST_ITEM)
+        items = call.data.get(SERVICE_LIST_ITEM)
 
-        # Split any things in the list separated by ' and '
-        things = [x for thing in things for x in thing.split(' and ')]
+        # Split any items in the list separated by ' and '
+        items = [x for item in items for x in item.split(' and ')]
 
         # Sync with Google servers
         keep.sync()
 
         list_to_update = _get_or_create_list_name_(list_name)
 
-        _LOGGER.info("Things to add: {}".format(things))
-        # For each thing,
-        for thing in things:
-            # ...is the thing already on the list?
-            for old_thing in list_to_update.items:
-                # Compare the new thing to each existing thing
-                if old_thing.text.lower() == thing.lower():
-                    # Uncheck the thing if it is already on the list.
-                    old_thing.checked = False
+        _LOGGER.info("items to add: {}".format(items))
+        # For each item,
+        for item in items:
+            # ...is the item already on the list?
+            for old_item in list_to_update.items:
+                # Compare the new item to each existing item
+                if old_item.text.lower() == item.lower():
+                    # Uncheck the item if it is already on the list.
+                    old_item.checked = False
                     break
-            # If the thing is not already on the list,
+            # If the item is not already on the list,
             else:
-                # ...add the thing to the list, unchecked.
-                list_to_update.add(thing, False)
+                # ...add the item to the list, unchecked.
+                list_to_update.add(item, False)
 
         # Sync with Google servers
         keep.sync()
-    
-    def sync_shopping_list(call):
-        """Get things from a Google Keep list."""
-
-        list_name = call.data.get(SERVICE_LIST_NAME, default_list_name)
-
-        # Sync with Google servers
-        keep.sync()
-
-        google_keep_list = _get_or_create_list_name_(list_name)
-        google_keep_list_items = []
-
-        for item in google_keep_list.items: google_keep_list_items.append({"text": item.text.lower().strip(), "checked": item.checked})
-        google_keep_list_items.sort(key=lambda x: x["text"])
-        google_keep_distinct_items = []
-
-        from itertools import groupby
-        for _, g in groupby(google_keep_list_items, key=lambda item: item["text"]):
-            group = list(g)
-            checked = [item for item in group if not item["checked"]]
-            thing = checked[0] if checked else group[0]
-            google_keep_distinct_items.append(thing)
-
-        google_keep_set = set([item["text"] for item in google_keep_distinct_items])
-        shopping_set = set([item["name"].lower().strip() for item in SHOPPING_LIST.items])
-
-        elements_to_add = google_keep_set - shopping_set
-        elements_to_check = google_keep_set & shopping_set
-
-        items_to_add = [item for item in google_keep_distinct_items if item["text"] in elements_to_add]
-        for item in items_to_add:
-            if not item["checked"]:
-                hass.services.call("shopping_list", 'add_item', {"name": item["text"]}, True)
-
-        items_to_update = [item for item in google_keep_distinct_items if item["text"] in elements_to_check]
-        for item in items_to_update:
-            if item["checked"]:
-                shopping_list_service = "complete_item"
-            else:
-                shopping_list_service = "incomplete_item"
-
-            hass.services.call("shopping_list", shopping_list_service, {"name": item["text"]}, True)
 
     def _get_or_create_list_name_(list_name):
         """ Find the target list amongst all the Keep notes/lists """
 
-        for l in keep.all():
-            if l.title == list_name:
-                list_object = l
+        for keep_list in keep.all():
+            if keep_list.title == list_name:
+                list_object = keep_list
                 break
         else:
             _LOGGER.info("List with name {} not found on Keep. Creating new list.".format(list_name))
             list_object = keep.createList(list_name)
-        
+
         return list_object
 
-
-    # Register the service google_keep.add_to_list with Home Assistant.
+    # Register the service gkeep_sync.add_to_list with Home Assistant.
     hass.services.register(DOMAIN, 'add_to_list', add_to_list, schema=SERVICE_LIST_SCHEMA)
-
-    # Register the service google_keep.sync_shopping_list with Home Assistant.
-    SHOPPING_LIST = hass.data.get(SHOPPING_LIST_DOMAIN)
-    if SHOPPING_LIST:
-        hass.services.register(DOMAIN, 'sync_shopping_list', sync_shopping_list, schema=SERVICE_LIST_NAME_SCHEMA)
 
     # Return boolean to indicate successful initialization.
     return True
